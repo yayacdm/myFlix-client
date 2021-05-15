fc342d595aae5b6f14660826729ece59afb3bc06
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-14 17:36:36 -0700

    added moviesmap to main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
 
 export class MainView extends React.Component {
 
@@ -20,7 +21,7 @@ export class MainView extends React.Component {
 
     return (
       <div className="main-view">
-        {movies.map(movie => <div key={movie._id}>{movie.Title}</div>)}
+        {movies.map(movie => <MovieCard key={movie._id} movie={movie} />)}
       </div>
     );
   }
