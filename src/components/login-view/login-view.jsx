8bc46e4bf1c028a0bfac9a462afd10c9d6bc3b42
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-18 21:38:55 -0700

    added login view props

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import './login-view.scss';
+import PropTypes from 'prop-types';
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
     /* Send a request to the server for authentication */
@@ -33,3 +34,12 @@ export function LoginView(props) {
 export function Button({ label }) {
   return <button className="login-button">{label}</button>;
 }
+
+LoginView.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }),
+  onLoggedIn: PropTypes.func.isRequired,
+  onRegister: PropTypes.func,
+};
