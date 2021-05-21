4ac3366ed6cdc06543fff5b57be618d4d07362f4
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-21 10:32:29 -0700

    added container wrapping

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainView from './components/main-view/main-view';
+import Container from 'react-bootstrap/Container';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
@@ -8,7 +9,11 @@ import './index.scss';
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return <MainView />;
+    return (
+      <Container>
+        <MainView />
+      </Container>
+    );
   }
 }
 
