d553d96f3de8d6d64ecc13e359126016b208d7cc
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-21 18:05:41 -0700

    adjusted movie card size on main view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -81,7 +81,7 @@ export class MainView extends React.Component {
             </Col>
           )
           : movies.map(movie => (
-            <Col md={3}>
+            <Col md={4}>
               <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie); }} />
             </Col>
           ))
