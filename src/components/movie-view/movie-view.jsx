72b825d1e128ce658bdd13a96ad1ec4ad312a289
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-21 17:08:42 -0700

    added margins to index

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
 
@@ -6,21 +7,23 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
+      <Jumbotron className="MovieView">
+        <div className="movie-view">
+          <div className="movie-poster">
+            <img src={movie.ImagePath} />
+          </div>
+          <div className="movie-title">
+            <span className="label">Title: </span>
+            <span className="value">{movie.Title}</span>
+          </div>
+          <div className="movie-description">
+            <span className="label">Description: </span>
+            <span className="value">{movie.Description}</span>
+          </div>
+          <button onClick={() => { onBackClick(null); }}>Back</button>
 
-      </div>
+        </div>
+      </Jumbotron>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container>
+      <Container className="Container">
         <MainView />
       </Container>
     );
