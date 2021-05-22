312b60c5cc20c7b058f20902a0ad5afe47c6ff07
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-21 17:48:45 -0700

    added styling to movie card

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import './movie-view.scss';
 
 export class MovieView extends React.Component {
 
