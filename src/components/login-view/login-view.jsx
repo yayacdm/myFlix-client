32d3fd8e41a46a9e785fbdba1a33faae8be0c747
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-24 07:29:44 -0700

    added axios handlesubmit

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,6 +3,7 @@ import './login-view.scss';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
@@ -10,10 +11,18 @@ export function LoginView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
     /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    axios.post('https://movieapi-yayacdm.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('no such user')
+      });
   };
 
   return (
