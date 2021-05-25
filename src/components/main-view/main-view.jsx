a0557cace1ad27ae7256dbafc3be86f91895bed8
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-24 20:30:27 -0700

    updated componentDidMount on mainview

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -20,15 +20,13 @@ export class MainView extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('https://movieapi-yayacdm.herokuapp.com/movies')
-      .then(response => {
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(error => {
-        console.log(error);
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
       });
+      this.getMovies(accessToken);
+    }
   }
 
   getMovies(token) {
