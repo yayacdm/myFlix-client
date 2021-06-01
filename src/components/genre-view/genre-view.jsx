9594e07b1a7c08a2f88c5720a04102bf4b799998
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-31 17:35:24 -0700

    removed div from genre view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -11,15 +11,13 @@ export class GenreView extends React.Component {
     const { genre, onBackClick } = this.props;
 
     return (
-      <div>
-        <Card border="info" bg="dark" text="white" className="genre-card">
-          <Card.Body>
-            <Card.Title><span className='text-primary'>Name: </span> {genre.Name}</Card.Title>
-            <Card.Text><span className='text-primary'>Bio: </span>{genre.Description}</Card.Text>
-            <Button block onClick={() => { onBackClick(); }}>Back</Button>
-          </Card.Body>
-        </Card>
-      </div>
+      <Card border="info" bg="dark" text="white" className="genre-card">
+        <Card.Body>
+          <Card.Title><span className='text-primary'>Name: </span> {genre.Name}</Card.Title>
+          <Card.Text><span className='text-primary'>Bio: </span>{genre.Description}</Card.Text>
+          <Button block onClick={() => { onBackClick(); }}>Back</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
