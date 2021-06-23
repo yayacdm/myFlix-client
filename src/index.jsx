68cc9f7807f1966069c3608304541023a8394294
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-06-23 09:52:06 -0700

    adjusted index.jsx and main-view to instructions, added movies-list

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainView from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+import MainView from './components/main-view/main-view';
 import './index.scss';
+
+const store = createStore(moviesApp, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container className="Container">
-        <MainView />
-      </Container>
+      <Provider store={store}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
 
 // Finds the root of your app
-const container = document.getElementById('app-container');
+const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render your app in the root DOM element
-ReactDOM.render(<MyFlixApplication />, container);
+ReactDOM.render(React.createElement(MyFlixApplication), container);
