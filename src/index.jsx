5313f136bbe46957fbf5e9c2191d8fea2d019d80
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-06-23 11:17:45 -0700

    updated users actions/reducers

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,11 @@ import Container from 'react-bootstrap/Container';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
-import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import MainView from './components/main-view/main-view';
+
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
 import './index.scss';
 
 const store = createStore(moviesApp, devToolsEnhancer());
@@ -25,7 +27,7 @@ class MyFlixApplication extends React.Component {
 }
 
 // Finds the root of your app
-const container = document.getElementsByClassName('app-container')[0];
+const container = document.getElementById('app-container');
 
 // Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
