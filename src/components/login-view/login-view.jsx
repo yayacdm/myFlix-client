3b5e1b648af18887101b3cc999febb3342a2c325
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-07-03 13:33:14 -0700

    required connect in login view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -5,6 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
