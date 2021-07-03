a41bf0bbb70bdb6ee0c2aa37d162d96c83d27522
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-07-03 14:17:07 -0700

    updated registration view to work

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -6,89 +6,65 @@ import './registration-view.scss';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [birthdate, setBirthdate] = useState('');
+  const validated = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let setisValid = formValidation();
-    if (setisValid) {
-      axios.post('https://movieapi-yayacdm.herokuapp.com/users', {
-        Username: username,
-        Password: password,
-        Email: email,
-        Birthdate: birthdate
+    axios.post('https://movieapi-yayacdm.herokuapp.com/users', {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthdate: birthdate
+    })
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        window.open('/', '_self'); // '_self' is necessary so that the page will open in the current tab
+        alert("You have sucessfully registered.");
       })
-        .then(response => {
-          const data = response.data;
-          console.log(data);
-          window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
-          alert("You have sucessfully registered.");
-        })
-        .catch(error => {
-          if (error.response && error.response.status === 400) {
-            alert('The value you entered is not valid.')
-          }
-        });
-      console.log(username, password, email, birthdate);
-      props.onRegister(username, password, email, birthdate);
-    };
-  }
-
-  const formValidation = () => {
-    const usernameError = {};
-    const emailError = {};
-    const passwordError = {};
-    const birhdateError = {};
-    let isValid = true;
-    if (username.trim().length < 5) {
-      usernameError.usernameShort = "Must be alphanumeric and contains at least 5 characters";
-      isValid = false;
-    }
-    else if (password.trim().length < 4) {
-      passwordError.passwordMissing = "You must enter a password.(minimum 4 characters) ";
-      isValid = false;
-    }
-    else if (!email.includes(".") || !email.includes("@")) {
-      emailError.emailNotEmail = "A valid email address is required.";
-      isValid = false;
-    }
-    else if (birthdate === '') {
-      birhdateError.noBirthdate = "Please enter a birthdate";
-      isValid = false;
-    }
-    return isValid;
+      .catch(error => {
+        if (error.response && error.response.status === 400) {
+          alert('The value you entered is not valid.')
+        }
+      });
+    console.log(username, password, email, birthdate);
   };
 
   return (
-    <Form className="RegForm" onSubmit={handleSubmit}>
+    <Form className="RegForm" onSubmit={handleSubmit} noValidate validated={validated}>
       <Form.Group controlId="formGroupUsername">
         <Form.Label>Username</Form.Label>
-        <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} required />
-        <Form.Control.Feedback type="invalid">Please provide a valid username.</Form.Control.Feedback>
+        <Form.Control type="text" placeholder="Enter Username" value={username} autoComplete="username" onChange={e => setUsername(e.target.value)} pattern='[a-zA-Z0-9]{5,}' minLength="5" required />
+        <Form.Control.Feedback type="invalid">Please provide a valid username at least 5 characters long.</Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="formGroupPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
-        <Form.Control.Feedback type="invalid">Please provide a valid password.</Form.Control.Feedback>
+        <Form.Control type="password" placeholder="Enter Password" value={password} autoComplete="password" onChange={e => setPassword(e.target.value)} minLength="5" required />
+        <Form.Control.Feedback type="invalid">Please provide a valid password at least 5 characters long.</Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="formGroupEmail">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required />
-        <Form.Control.Feedback type="invalid">Please provide a valid email.</Form.Control.Feedback>
+        <Form.Control type="email" placeholder="Enter Email" value={email} autoComplete="email" onChange={e => setEmail(e.target.value)} required />
+        <Form.Control.Feedback type="invalid">Please provide a valid email address.</Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="formGroupBirthdate">
         <Form.Label>Birthdate</Form.Label>
         <Form.Control type="date" placeholder="00-00-0000" value={birthdate} onChange={e => setBirthdate(e.target.value)} required />
+        <Form.Control.Feedback type='invalid'>Please enter a valid birthday.</Form.Control.Feedback>
       </Form.Group>
       <span>
-        <Button type="submit">Submit</Button>
+        <Button type="submit" onClick={handleSubmit}>Submit</Button>
         {' '}
-        <Button variant="secondary" onClick={history.goBack}>Back</Button>
+        <Link to="/">
+          <Button variant="secondary" type="button">Back</Button>
+        </Link>
       </span>
     </Form >
   )
